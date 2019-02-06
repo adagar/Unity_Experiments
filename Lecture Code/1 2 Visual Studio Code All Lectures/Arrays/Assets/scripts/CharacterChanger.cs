512b99c450c415ac05bd06e44baf74332710bbe1
adagar <adagar121987@gmail.com>
2019-02-05 19:46:26 -0500

    Updated Last project to use array of game objects instead of baked empty fields

---
+++ Lecture Code/1 2 Visual Studio Code All Lectures/Arrays/Assets/scripts/CharacterChanger.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Changes character on left mouse button
+/// </summary>
+public class CharacterChanger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] prefabCharacters = new GameObject[4];
+
+    // need for location of new character
+    GameObject currentCharacter;
+
+    // first frame input support
+    bool previousFrameChangeCharacterInput = false;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        currentCharacter = Instantiate<GameObject>(
+            prefabCharacters[0], Vector3.zero,
+            Quaternion.identity);
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // change character on left mouse button
+        if (Input.GetAxis("ChangeCharacter") > 0)
+        {
+            // only change character on first input frame
+            if (!previousFrameChangeCharacterInput)
+            {
+                previousFrameChangeCharacterInput = true;
+
+                // save current position and destroy current character
+                Vector3 position = currentCharacter.transform.position;
+                Destroy(currentCharacter);
+
+                // instantiate a new random character
+                int prefabNumber = Random.Range(0, 4);
+                if (prefabNumber == 0)
+                {
+                    currentCharacter = Instantiate(prefabCharacters[0], 
+                        position, Quaternion.identity);
+                }
+                else if (prefabNumber == 1)
+                {
+                    currentCharacter = Instantiate(prefabCharacters[1], 
+                        position, Quaternion.identity);
+                }
+                else if (prefabNumber == 2)
+                {
+                    currentCharacter = Instantiate(prefabCharacters[2], 
+                        position, Quaternion.identity);
+                }
+                else
+                {
+                    currentCharacter = Instantiate(prefabCharacters[3], 
+                        position, Quaternion.identity);
+                }
+            }
+        }
+        else
+        {
+            // no change character input
+            previousFrameChangeCharacterInput = false;
+        }
+    }
+}
